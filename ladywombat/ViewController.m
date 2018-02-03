94e3fb74955fd3bb257170dc26e18e2c3ad258f8
Roman Mykitchak <deviphone@i.ua>
2018-02-03 12:54:43 +0100

    Initial Commit

---
+++ ladywombat/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ladywombat
+//
+//  Created by Roman Mykitchak on 2/3/18.
+//  Copyright © 2018 Roman Mykitchak. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ ladywombat/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ladywombat
+//
+//  Created by Roman Mykitchak on 2/3/18.
+//  Copyright © 2018 Roman Mykitchak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
